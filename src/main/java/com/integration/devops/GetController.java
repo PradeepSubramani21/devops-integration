097cd75fdad9db3b5c8e070dcd65b3af35d05281
PradeepSubramani21 <contactpradeepsubramani@gmail.com>
2022-12-26 20:27:01 +0530

    test changes

--- src/main/java/com/integration/devops/GetController.java
+++ src/main/java/com/integration/devops/GetController.java
@@ -8,6 +8,12 @@ public class GetController {
 
 	@GetMapping("/")
 	public String getValue() {
+		System.out.println("***** getValue *******");
 		return "Greetings from Pradeep !";
+	}
+
+	@GetMapping("/hi")
+	public String getOptions(){
+		return "Hey !!";
 	}
 }
